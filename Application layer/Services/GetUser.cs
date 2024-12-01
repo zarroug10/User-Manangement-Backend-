3e63ba176a6aab0dab69d6222cd4558dbfe26c26
Zarroug abdelhafidh <115534999+zarroug10@users.noreply.github.com>
2024-12-01 06:17:22 +0100

    Update GetUser.cs

--- Application layer/Services/GetUser.cs
+++ Application layer/Services/GetUser.cs
@@ -7,13 +7,6 @@ public class GetUser
     {
         _userRepository = userRepository;
     }
-
-    public async Task CreateUserAsync(string username, string email, string password, UserRole role)
-    {
-        var user = new User(username, email, password, role);
-        await _userRepository.SaveAsync(user);
-    }
-
     public async Task<User> GetUserByIdAsync(int id)
     {
         return await _userRepository.GetByIdAsync(id);
